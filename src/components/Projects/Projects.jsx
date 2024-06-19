c293f702f4e3bd8e0cef139d5904b0ea36189314
Sukanya <sukanya.s1793@gmail.com>
2024-06-19 02:48:47 -0500

    chages

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styles from "./Projects.module.css";
+export const Projects = () => {
+    return (
+    <section>
+        <h2>
+            Projects
+        </h2>
+        <div>
+            {
+                Pr
+            }
+        </div>
+    </section>
+    )
+}
